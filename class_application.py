89a9b2eeb92f9785758e65cf43538264937ebfed
Kimura.Tetsuya <t.kimura@axelmark.co.jp>
2017-01-16 18:04:22 +0900

    学習進捗　day3

---
+++ class_application.py
@@ -0,0 +1,34 @@
+class Calculation:
+    value = 0
+    def square(self):
+        s = self.value * self.value
+        return s
+    def tri(self):
+        y = self.value * self.value * self.value
+        return y
+
+calcs = [Calculation(),Calculation(),Calculation()]
+#cal = [Caluculationses(),Caluculationses(),Caluculationses()]
+
+calcs[0].value = 3
+calcs[1].value = 5
+calcs[2].value = 7
+#cal[0].value = 3
+#cal[1].value = 4
+#cal[2].value = 5
+
+print calcs[0].square()
+print calcs[1].square()
+print calcs[2].square()
+print calcs[0].tri()
+print calcs[1].tri()
+print calcs[2].tri()
+
+for c in calcs:
+    print c.square()
+    print c.tri()
+
+    
+
+
+
--- list_in_list.py
+++ list_in_list.py
@@ -1,4 +1,10 @@
 a =[[2012, 2013, 2014],[2015, 2016, 2017]]
-
 b = a[1][0]
-print b　
+print b
+
+a.append([2018,2019])
+print a
+
+a[2].append(2020)
+print a
+
